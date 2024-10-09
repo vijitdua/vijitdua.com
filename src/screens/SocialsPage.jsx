261daff562af0cecd9d877efa1fd8a70a6be0105
vijitdua <82555472+vijitdua@users.noreply.github.com>
2024-10-09 20:05:58 +0800

    Projects page not ready

--- src/screens/SocialsPage.jsx
+++ src/screens/SocialsPage.jsx
@@ -7,6 +7,7 @@ import InfoIcon from '@mui/icons-material/Info';
 import {bio} from "../configs/misc";
 import MenuItemsWithIcons from "../components/MenuItems/MenuItemsWithIcons";
 import MailIcon from "@mui/icons-material/Mail";
+import MainLayout from "../layouts/MainLayout";
 
 function Section({title, items}) {
     return (<Box sx={{
@@ -39,62 +40,67 @@ function Section({title, items}) {
 
 function SocialsPage() {
     return (
-        <Container maxWidth="md">
+        <MainLayout>
+            <Container maxWidth="md">
 
-            {/* Profile picture and bio */}
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '1rem',
-                mb: '1rem',
-            }}>
-                <Avatar alt="Vijit Dua" src={`${process.env.PUBLIC_URL}/assets/pfp.png`} sx={{
-                    height: '7rem',
-                    width: '7rem',
-                    boxShadow: '0.4rem 0.4rem',
-                }}/>
-                <Typography variant='h3' component='h1'> Vijit Dua </Typography>
+                {/* Profile picture and bio */}
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    mb: '1rem',
+                }}>
+                    <Avatar alt="Vijit Dua" src={`${process.env.PUBLIC_URL}/assets/pfp.png`} sx={{
+                        height: '7rem',
+                        width: '7rem',
+                        boxShadow: '0.4rem 0.4rem',
+                    }}/>
+                    <Typography variant='h3' component='h1'> Vijit Dua </Typography>
 
-                <Typography
-                    variant="body1"
-                    component="body1"
-                    sx={{
-                        maxWidth: '30rem', // Set your desired maximum width
-                        margin: '0 auto', // Center the text horizontally
-                        textAlign: 'center', // Center the text within each line
-                        wordWrap: 'break-word', // Ensure words break and wrap to the next line if necessary
-                    }}
-                >
-                    {bio}
-                </Typography>
+                    <Typography
+                        variant="body1"
+                        component="body1"
+                        sx={{
+                            maxWidth: '30rem', // Set your desired maximum width
+                            margin: '0 auto', // Center the text horizontally
+                            textAlign: 'center', // Center the text within each line
+                            wordWrap: 'break-word', // Ensure words break and wrap to the next line if necessary
+                        }}
+                    >
+                        {bio}
+                    </Typography>
 
-                <SocialMediaOvalItem
-                    item={{name: 'About Me', icon: <InfoIcon/>, route: routes.aboutMe,}} index={1}
-                    key={1}/>
-            </Box>
+                    <SocialMediaOvalItem
+                        item={{name: 'About Me', icon: <InfoIcon/>, route: routes.aboutMe,}} index={1}
+                        key={1}/>
+                </Box>
 
-            {/* Sections */}
-            <Section title='Content' items={contentList}/>
+                {/* Sections */}
+                <Section title='Content' items={contentList}/>
 
-            <Section title='Socials' items={socialsList}/>
+                <Section title='Socials' items={socialsList}/>
 
-            <Section title='Professional' items={professionalSocials}/>
+                <Section title='Professional' items={professionalSocials}/>
 
-            {/* Mail icon */}
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '1rem',
-                mb: '1rem',
-            }}>
-                {[{icon: <MailIcon/>, route: socialLinks.email,}].map((item, index) => MenuItemsWithIcons(item, index))}
-            </Box>
+                {/* Mail icon */}
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    mb: '1rem',
+                }}>
+                    {[{
+                        icon: <MailIcon/>,
+                        route: socialLinks.email,
+                    }].map((item, index) => MenuItemsWithIcons(item, index))}
+                </Box>
 
-        </Container>
+            </Container>
+        </MainLayout>
     );
 }
 
