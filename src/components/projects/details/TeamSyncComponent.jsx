26a002e08c9ed1d40ec6fcaa3ab5b0d8a2af5ed0
vijitdua <82555472+vijitdua@users.noreply.github.com>
2024-10-11 17:02:50 +0800

    More data, more media. Dividers

--- src/components/projects/details/TeamSyncComponent.jsx
+++ src/components/projects/details/TeamSyncComponent.jsx
@@ -1,37 +1,56 @@
 import React from 'react';
 import {Typography, Box, Link} from '@mui/material';
 import MultimediaCarousel from "../../collections/MultimediaCarousel";
+import Divider from "@mui/material/Divider";
 
 function TeamSyncComponent() {
     return (
-        <Box sx={{ maxWidth: '800px', textAlign: 'justify' }}>
+        <Box sx={{maxWidth: '800px', textAlign: 'justify'}}>
 
             <Typography variant="h4" gutterBottom>
                 About Team Sync
             </Typography>
             <Typography variant="body1" paragraph>
-                TeamSync is a project aimed at streamlining administrative tasks & member management for Discord-based teams.
+                TeamSync is a project aimed at streamlining administrative tasks & member management for Discord-based
+                teams.
             </Typography>
+
+            <Box sx={{ width: '100%' }}>
+                <Divider variant="middle" sx={{mb:'1rem'}}/>
+            </Box>
 
             <Typography variant="body1" paragraph>
                 <strong>Details / Technology</strong>
             </Typography>
             <ul>
                 <li>Built in collaboration in a team of 2*</li>
-                <li>Implemented RESTful APIs and enabled public data display through simple GET requests for web integrations.</li>
+                <li>Implemented RESTful APIs and enabled public data display through simple GET requests for web
+                    integrations.
+                </li>
                 <li>Created a React.js web client, Discord.js Discord bot, and a Node.js Express.js backend.</li>
                 <li>Additional Utilities: Passport.js, Sequalize.js</li>
             </ul>
 
             <Typography variant='body1' paragraph>
-                *Built by Vijit Dua, and <Link href={`https://linkedin.com/in/naomi-zhao-b80375253`}> Naomi Zhao</Link>
+                *Built by Vijit Dua, and <Link href={`https://linkedin.com/in/naomi-zhao-b80375253`}
+                                               target={"_blank"}
+                                               rel={"noopener noreferrer"}> Naomi Zhao</Link>
             </Typography>
+
+            <Box sx={{ width: '100%' }}>
+                <Divider variant="middle" sx={{mb:'1rem'}}/>
+            </Box>
 
             <Typography variant="body1" paragraph>
-                <strong>Media</strong>
+                <strong>In-Progress</strong>
             </Typography>
+            <Typography varient={'body1'} paragraph> Media Coming soon. </Typography>
 
-            <MultimediaCarousel/>
+            {/*<MultimediaCarousel/>*/}
+            <Box sx={{ width: '100%' }}>
+                <Divider variant="middle" sx={{mb:'1rem'}}/>
+            </Box>
+
         </Box>
     );
 }
--- src/components/projects/details/TicTacToeComponent.jsx
+++ src/components/projects/details/TicTacToeComponent.jsx
@@ -1,11 +1,24 @@
 import {Box, Typography} from "@mui/material";
 import MultimediaCarousel from "../../collections/MultimediaCarousel";
 import React from "react";
+import Divider from "@mui/material/Divider";
+
+const multimediaData = [
+    { type: 'image', url: `${process.env.PUBLIC_URL}/assets/projects/tic-tac-toe/sign-up.png` },
+    { type: 'image', url: `${process.env.PUBLIC_URL}/assets/projects/tic-tac-toe/menu.png` },
+    { type: 'image', url: `${process.env.PUBLIC_URL}/assets/projects/tic-tac-toe/waiting.png` },
+    { type: 'image', url: `${process.env.PUBLIC_URL}/assets/projects/tic-tac-toe/play-1.png` },
+    { type: 'image', url: `${process.env.PUBLIC_URL}/assets/projects/tic-tac-toe/victory.png` },
+];
 
 function TicTacToeComponent(){
     return (
         <Box sx={{ maxWidth: '800px', textAlign: 'justify' }}>
 
+
+            <Typography variant="h4" gutterBottom>
+                About Multiplayer TicTacToe
+            </Typography>
             <Typography variant="body1" paragraph>
                 TicTacToe Multiplayer Game is a web-based game that allows players to compete in real-time. I built it as a way to dive headfirst into both frontend and backend development, using this project to rapidly learn the basics of web technologies.
             </Typography>
@@ -18,6 +31,9 @@ function TicTacToeComponent(){
                 The game provided a simple yet effective learning opportunity, helping me understand how to create interactive applications while marking my transition from CLI-based projects to web development. It became a stepping stone in my journey towards full-stack development.
             </Typography>
 
+            <Box sx={{ width: '100%' }}>
+                <Divider variant="middle" sx={{mb:'1rem'}}/>
+            </Box>
 
             <Typography variant="body1" paragraph>
                 <strong>Details / Technology </strong>
@@ -28,11 +44,20 @@ function TicTacToeComponent(){
                 <li>DevOps & hosting: AWS EC2, Docker, NGINX, Certbot (SSL), AWS Route 53 (DNS).</li>
             </ul>
 
+            <Box sx={{ width: '100%' }}>
+                <Divider variant="middle" sx={{mb:'1rem'}}/>
+            </Box>
+
             <Typography variant="body1" paragraph>
                 <strong>Media</strong>
             </Typography>
 
-            <MultimediaCarousel/>
+            <MultimediaCarousel data={multimediaData}/>
+
+            <Box sx={{ width: '100%' }}>
+                <Divider variant="middle" sx={{mb:'1rem'}}/>
+            </Box>
+
         </Box>
     );
 }
