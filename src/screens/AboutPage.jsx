ab1423eb4c6ebd96b71b7623ebd9af79c97b8ae3
vijitdua <82555472+vijitdua@users.noreply.github.com>
2024-10-11 18:53:01 +0800

    Everything done, individual experiences left.

--- src/screens/AboutPage.jsx
+++ src/screens/AboutPage.jsx
@@ -5,6 +5,8 @@ import {Helmet} from "react-helmet";
 import {env} from "../configs/envConfig";
 import {routes, socialLinks} from "../configs/routesConfig";
 import {theme} from "../themes/primaryTheme";
+import Quote from "../components/typography/Quote";
+import Collapsible from "../components/typography/Collapsible";
 
 
 function Introduction({mobileView}) {
@@ -85,23 +87,214 @@ function AboutPage() {
     return (
         <MainLayout>
             <PageMeta/>
-            <Container maxWidth="xl">
+            <Container maxWidth="lg">
 
-                <Box sx={{
-                    '& > *': {
-                        m: mobileView ? '1rem 0' : '2.5rem 0',
-                    }
-                }}>
-
-                    <Typography variant="h1" gutterBottom sx={{textAlign: 'center'}}>
+                <Box sx={{mt: '3rem', mb: '2rem'}}>
+                    <Typography variant="h1" gutterBottom sx={{textAlign: 'center', mb: '3rem'}}>
                         About Me
                     </Typography>
 
-                    <Introduction mobileView={mobileView}/>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: mobileView ? 'column' : 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            gap: '2rem',
+                        }}
+                    >
+                        <Avatar
+                            alt="Vijit Dua"
+                            src={`${process.env.PUBLIC_URL}/assets/pfp.png`}
+                            sx={{
+                                height: mobileView ? '10rem' : '16rem',
+                                width: mobileView ? '10rem' : '16rem',
+                            }}
+                        />
 
-                    <Divider/>
-
+                        <Box sx={{flexGrow: 1}}>
+                            <Typography variant="h4" gutterBottom>
+                                Who is Vijit?
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                I'm Vijit, a CS student with a side quest in entrepreneurship. I'm all
+                                about blending coding with my dream to kick off something cool on my
+                                own. Outside of plotting to take over the tech world, you'll catch me
+                                hitting the pavement on my runs, flexing at the gym, crafting videos /
+                                edits for my{' '}
+                                <Link href="https://youtube.com/@vijitdua" target="_blank">
+                                    YouTube channel
+                                </Link>
+                                , or sometimes (rarely) even producing music.
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                When I'm not cooped up being productive, I tend to be an extremely
+                                outgoing person. I live for the thrill of meeting new folks and jumping
+                                into any convo, from debating the existential status of hotdogs
+                                (definitely sandwiches, right?) to unraveling the mysteries of physics.
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                I'm also a part-time gamer. Mostly into BeatSaber, but anything that
+                                runs on a Mac or Oculus Quest 2 is cool by me.
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Box>
+
+                <Divider/>
+
+                {/* Quotes Section */}
+                <Box sx={{mt: '3rem', mb: '2rem'}}>
+                    <Typography variant="h4" gutterBottom>
+                        Quotes
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        As I go through life, I like to try to live by certain principles / beliefs. All
+                        of these are self-written and keep getting updated here and there. (Click “⏵” to
+                        expand)
+                    </Typography>
+                    <Quote quote="Complaining doesn’t help, finding a solution does (even in unfair situations)."/>
+                    <Collapsible title="More quotes">
+                        <Quote
+                            quote="Hold yourself accountable for everything. Even if it’s not your fault, you are the one who has to change the circumstances for yourself."/>
+                        <Quote
+                            quote="It is what it is. Look for the next best thing to do, learn, and move on — don’t pointlessly dwell on what’s out of your control. Or if it’s in your control, find a solution."/>
+                        <Quote quote="Responses to scenarios matter more than the scenarios."/>
+                        <Quote
+                            quote="Hold your beliefs strongly while equally testing other schools of thought. More often than not, the more seemingly absurd things turn out true."/>
+                        <Quote
+                            quote="Form your own postulates, & question everything. Go through pre-existing notions but test them instead of blind faith. We are humored by the progress we had millennia ago (witch burnings? Seriously?) who says our present situation won’t be similarly hilarious in retrospect?"/>
+                        <Quote
+                            quote="The direct/indirect results of your actions should be persuasive enough to convince you to continue if you truly desire it. Embrace discipline, not motivation."/>
+                        <Quote
+                            quote="Slow change isn’t effective, fix the problems within yourself instantly. Step outside your comfort zones if you want to work on yourself."/>
+                        <Quote quote="Luck only favors you if you capitalize it."/>
+                        <Quote
+                            quote="Efforts ≠ Merits. You need to put effort into your work, but you also need to put efforts into analyzing your efforts."/>
+                        <Quote
+                            quote="Life is recursive. If you can't strategize how to approach a problem, strategize the strategizing. The solution is just out of your vision, not impossible."/>
+                        <Quote
+                            quote="No statement applies to every circumstance, but they may apply to the majority. Stop spotting exceptional situations where the statement fails. The fact you know the exceptional scenarios means you also know what the correct scenarios are. Apply the statements where they fit."/>
+                        <Quote quote="n e v e r    s e t t l e"/>
+                    </Collapsible>
+                </Box>
+
+                <Divider/>
+
+                {/* Goals Section */}
+                <Box sx={{mt: '3rem', mb: '2rem'}}>
+                    <Typography variant="h4" gutterBottom>
+                        My Goals in Life
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        At multiple points of time in my life I’ve tried to justify the meaning of life
+                        and existence. If we all die at some point, why does it really even matter?
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        This is something I haven’t quite figured out yet, but at the moment I have
+                        justified this by giving myself two life-long goals that go hand in hand with
+                        each other: career goals and personal goals.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        I’ve tried to isolate, to some extent, the meaning of my life to my ideals and
+                        principles. Instead of thinking of myself individualistically (in the long term),
+                        I prefer to think of myself as a cog in the bigger machine of the human race and
+                        its evolution (evolution of both knowledge & genetics). With my goal being to
+                        just be the best cog I can. A cog that never settles, even if I somehow make it
+                        to the top.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        This gives me a bit of a trajectory. Though it still doesn’t directly answer my
+                        former question regarding the meaning of life.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        I believe that what makes a person, a person, is their ways of living their
+                        life. Their thought process and decision making. Let’s extend this idea into us
+                        just slowly changing the evolution of ideas of the human race and detach
+                        yourself from your own intrinsic meaning.
+                    </Typography>
+
+                    {/* Optional Collapsible analogy */}
+                    <Collapsible
+                        title="Click to expand this if you want an analogy to better understand my thought process.">
+                        <Typography variant="body1" paragraph>
+                            If you don’t believe in souls, what would make you different from a clone of
+                            you with the exact same memories as you? Nothing, right? That’s my point.
+                            What makes a person, a person, is their ways of living their life. Their
+                            thought process and decision making. (And of course, their experiences and
+                            memories.)
+                        </Typography>
+                    </Collapsible>
+
+                    <Typography variant="body1" paragraph>
+                        With that, I have come to the temporary conclusion that my meaning in life is to
+                        essentially aid towards the development of people in general. Pass on my ideals
+                        and way in life, and that way be “immortalized” even after death to somewhat of
+                        an extent.
+                    </Typography>
+
+                    <Typography variant="h5" gutterBottom sx={{mt: '2rem'}}>
+                        Personal Goal
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        My personal goal is to constantly instigate, in whatever capacity, positive
+                        changes around myself everywhere I go.
+                    </Typography>
+
+                    <Typography variant="h5" gutterBottom sx={{mt: '2rem'}}>
+                        Career Goal
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Ultimately, my goal is to either lead a startup or hold a significant position in
+                        a company that aligns with my interests. I envision a role where my decisions
+                        influence the trajectory of the company and lead to the betterment of society.
+                        However, this is just a tangential plan towards my final objective. My true aim
+                        is to consistently chase the “next best thing” in every moment and “never
+                        settle”, adapting in response to the opportunities I encounter.
+                    </Typography>
+
+                    <Typography variant="h5" gutterBottom sx={{mt: '2rem'}}>
+                        How They Intertwine
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        The goal in both cases is to eventually lead to positive change and/or
+                        development, and to constantly strive to achieve that to a greater extent.
+                    </Typography>
+                </Box>
+
+                <Divider/>
+
+                {/* Interesting Question Section */}
+                <Box sx={{mt: '3rem', mb: '2rem'}}>
+                    <Typography variant="h4" gutterBottom>
+                        An Interesting Question
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        An interesting question that I ask myself, and would like to ask others is, if
+                        you could only choose one, which would you choose?
+                    </Typography>
+                    <Quote
+                        quote="Do you want to live a life to remember, or live a life to be remembered? Or perhaps lead to progress without being remembered…"/>
+                    <Typography variant="body1" paragraph>
+                        Of course, this is just a hypothetical question. I believe that it is possible
+                        to choose at least two of these at the same time. Although if you choose two, it
+                        comes at the cost of one of those not being as satisfactory as the other.
+                    </Typography>
+                </Box>
+
+                <Divider/>
+
+                {/* Closing Remark */}
+                <Box sx={{mt: '3rem', mb: '2rem', textAlign: 'center'}}>
+                    <Typography variant="h5" gutterBottom>
+                        Anyways, that’s who I am. I am Vijit.
+                    </Typography>
+                    <Typography variant="h5" gutterBottom>
+                        Thanks for taking the time to get to know me :)
+                    </Typography>
+                </Box>
+
+                <Divider/>
 
             </Container>
         </MainLayout>
--- src/screens/PortfolioPage.jsx
+++ src/screens/PortfolioPage.jsx
@@ -1,8 +1,13 @@
 import {Helmet} from "react-helmet";
 import {env} from "../configs/envConfig";
 import {routes} from "../configs/routesConfig";
-import {Container} from "@mui/material";
+import {Box, Container, Grid, Typography, useMediaQuery} from "@mui/material";
 import MainLayout from "../layouts/MainLayout";
+import IntroductionSection from "../components/home/IntroductionSection";
+import ProfileSection from "../components/home/ProfileSection";
+import ExperienceTimeline from "../components/experience/ExperienceTimeline";
+import ProjectsCollection from "../components/projects/ProjectsCollection";
+import {theme} from "../themes/primaryTheme";
 
 function PageMeta() {
     return (
@@ -17,13 +22,41 @@ function PageMeta() {
     );
 }
 
-function PortfolioPage(){
+function PortfolioPage() {
+    const mobileView = useMediaQuery(theme.breakpoints.down("md"));
     return (
         <MainLayout>
             <PageMeta/>
-            <Container maxWidth="xl">
+            <Container maxWidth="xl" sx={{marginTop: '4rem'}}>
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: mobileView ? 'column-reverse' : 'row',
+                    justifyContent: mobileView ? 'center' : 'space-around',
+                    alignItems: mobileView ? 'center' : 'center',
+                    width: '100%',
+                    gap: '3rem',
+                }}>
+                    <Grid item xs={12} md={6}>
+                        <IntroductionSection/>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <ProfileSection/>
+                    </Grid>
+                </Box>
 
+                <Container maxWidth="lg">
+                    <Typography variant="h1" gutterBottom sx={{ textAlign: 'center' }}>
+                        Experience
+                    </Typography>
+                    <ExperienceTimeline/>
+                </Container>
 
+                <Container maxWidth="lg">
+                    <Typography variant="h1" gutterBottom sx={{ textAlign: 'center' }}>
+                        Projects
+                    </Typography>
+                    <ProjectsCollection/>
+                </Container>
 
             </Container>
         </MainLayout>
