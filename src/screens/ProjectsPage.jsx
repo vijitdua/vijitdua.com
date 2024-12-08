73211ffc2f968d440b87f39de1543c3e3c2c023e
vijitdua <82555472+vijitdua@users.noreply.github.com>
2024-12-08 23:27:56 +0800

    feat: TOC (kinda eh so will remove but version tracking)

--- src/screens/ProjectsPage.jsx
+++ src/screens/ProjectsPage.jsx
@@ -3,7 +3,18 @@ import ProjectsCollection from '../components/projects/ProjectsCollection';
 import {Helmet} from "react-helmet";
 import {env} from "../configs/envConfig";
 import {routes} from "../configs/routesConfig";
-import {Container, Divider, Typography} from "@mui/material";
+import {
+    Box,
+    Container,
+    Divider,
+    Grid,
+    List,
+    ListItem,
+    ListItemButton, ListItemText,
+    Typography,
+    useMediaQuery,
+    useTheme
+} from "@mui/material";
 import ServiceStatus from "../components/service-and-bugs/ServiceStatus";
 import BugReport from "../components/service-and-bugs/BugReport";
 import LiveProjectsList from "../components/projects/LiveProjectsList";
@@ -20,6 +31,68 @@ function PageMeta() {
                 content="Gallery of projects made by Vijit Dua."
             />
         </Helmet>
+    );
+}
+
+function TableOfContents({children}) {
+    const theme = useTheme();
+    const isNotDesktop = useMediaQuery(theme.breakpoints.down('lg'));
+
+    const widthOfContent = '13rem';
+
+    return (
+        <Box sx={{
+            height: '100vh',
+        }}>
+            {!isNotDesktop && (
+                <Box sx={{
+                    mt: '10rem',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '1rem',
+                    maxWidth: widthOfContent,
+                    position: 'fixed',
+                    overflowY: 'hidden',
+                    overflow: 'hidden',
+                }}>
+                    <Box>
+                        <List>
+
+                            <Typography variant="h4" sx={{marginLeft: '1rem'}}>Contents</Typography>
+
+                            <ListItem disablePadding>
+                                <ListItemButton component="a" href="#live">
+                                    <ListItemText primary="Live Projects"/>
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding>
+                                <ListItemButton component="a" href="#all">
+                                    <ListItemText primary="All Projects"/>
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding>
+                                <ListItemButton component="a" href="#status">
+                                    <ListItemText primary="Service Status"/>
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding>
+                                <ListItemButton component="a" href="#report">
+                                    <ListItemText primary="Bug Report"/>
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+                    </Box>
+                </Box>
+            )}
+            <Box sx={{
+                display: 'flex',
+                marginLeft: isNotDesktop ? '0' : widthOfContent,
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+            }}>
+                {children}
+            </Box>
+        </Box>
     );
 }
 
@@ -46,37 +119,40 @@ function ProjectsPage() {
     return (
         <MainLayout>
             <PageMeta/>
-            <Container maxWidth="xl" sx={{marginBottom: '5rem'}}>
 
-                <Typography variant='h1' align='center'>
-                    Projects
-                </Typography>
+            <TableOfContents>
+                <Container id='live' maxWidth="xl" sx={{marginTop: '3rem', marginBottom: '2rem'}}>
+                    <LiveProjectsList/>
+                </Container>
 
-                <ProjectsCollection/>
+                <Divider/>
 
-            </Container>
+                <Container id='all' maxWidth="xl" sx={{marginBottom: '5rem'}}>
 
-            <Divider/>
+                    <Typography variant='h1' align='center'>
+                        All Projects
+                    </Typography>
 
-            <Container id='live' maxWidth="xl" sx={{marginTop: '3rem', marginBottom: '2rem'}}>
-                <LiveProjectsList/>
-            </Container>
+                    <ProjectsCollection/>
 
-            <Divider/>
+                </Container>
 
-            {/* Service Status Section */}
-            <Container id="status" maxWidth="xl" sx={{marginTop: '3rem', marginBottom: '3rem'}}>
-                <ServiceStatus/>
-            </Container>
+                <Divider/>
 
-            <Divider/>
+                {/* Service Status Section */}
+                <Container id="status" maxWidth="xl" sx={{marginTop: '3rem', marginBottom: '3rem'}}>
+                    <ServiceStatus/>
+                </Container>
 
-            {/* Bug Report Section */}
-            <Container id="report" maxWidth="xl" sx={{marginTop: '3rem', marginBottom: '5rem'}}>
-                <BugReport/>
-            </Container>
+                <Divider/>
 
-            <Divider/>
+                {/* Bug Report Section */}
+                <Container id="report" maxWidth="xl" sx={{marginTop: '3rem', marginBottom: '5rem'}}>
+                    <BugReport/>
+                </Container>
+
+                <Divider/>
+            </TableOfContents>
 
         </MainLayout>
     );
